dd8b2c0402d4b5cdbe31a87e8f9de1e2a48e0c63
MohamedAhmedGameel <mohamedahmedgameel@gmail.com>
2025-11-26 15:00:29 +0200

    patch swagger

---
+++ docs/components.js
@@ -0,0 +1,18 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *         googleId:
+ *           type: string
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         avatar:
+ *           type: string
+ */
